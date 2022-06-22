fd5096a6139e8432161a5a61017230f2ffa7a326
karuiisi <85538968+karuiisi@users.noreply.github.com>
2022-06-22 09:50:30 +0900

    変更

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
 // TODO: 公式リファレンスに沿って JavaScript を記述
-const HelloVueApp = {
+const Counter = {
+  data(){
+    return{
+      counter:0
+    }
+  },
+  mounted() {
+    setInterval(() => {
+      this.counter++
+    }, 1000)
+  }
+}
+const AttributeBinding = {
   data() {
     return {
-      message: 'Hello Vue!!'
-      counter:10
+      message: 'You loaded this page on ' + new Date().toLocaleString()
+    }
+  }
+}
+const EventHandling = {
+  data() {
+    return {
+      message: 'Hello Vue.js!'
+    }
+  },
+  methods: {
+    reverseMessage() {
+      this.message = this.message
+        .split('')
+        .reverse()
+        .join('')
     }
   }
 }
 
+Vue.createApp(EventHandling).mount('#event-handling')
+Vue.createApp(AttributeBinding).mount('#bind-attribute')
 Vue.createApp(HelloVueApp).mount('#hello-vue')
