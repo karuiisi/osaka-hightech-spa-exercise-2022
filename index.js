5386a8a3a424a074b32f2bd9c39f9f65b5b7e62b
karuiisi <85538968+karuiisi@users.noreply.github.com>
2022-06-22 10:09:52 +0900

    変更

--- index.js
+++ index.js
@@ -1,23 +1,3 @@
-// TODO: 公式リファレンスに沿って JavaScript を記述
-const Counter = {
-  data(){
-    return{
-      counter:0
-    }
-  },
-  mounted() {
-    setInterval(() => {
-      this.counter++
-    }, 1000)
-  }
-}
-const AttributeBinding = {
-  data() {
-    return {
-      message: 'You loaded this page on ' + new Date().toLocaleString()
-    }
-  }
-}
 const EventHandling = {
   data() {
     return {
@@ -35,5 +15,3 @@ const EventHandling = {
 }
 
 Vue.createApp(EventHandling).mount('#event-handling')
-Vue.createApp(AttributeBinding).mount('#bind-attribute')
-Vue.createApp(HelloVueApp).mount('#hello-vue')
