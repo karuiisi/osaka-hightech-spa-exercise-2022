9eaed360e0e325ef30416c27088d0181ebd419d0
karuiisi <85538968+karuiisi@users.noreply.github.com>
2022-06-22 09:35:56 +0900

    hennkou

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // TODO: 公式リファレンスに沿って JavaScript を記述
+const HelloVueApp = {
+  data() {
+    return {
+      message: 'Hello Vue!!'
+      counter:10
+    }
+  }
+}
+
+Vue.createApp(HelloVueApp).mount('#hello-vue')
